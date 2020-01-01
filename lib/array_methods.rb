adca4f6bedd2247013dad52667432f12e1af21fe
anam soomro <anamsoomroed@gmail.com>
2020-01-01 19:25:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,9 +13,9 @@ end
 #end
 
 def find_min_value(array)
-  array.length.times do { |index|
-   if array[index] > x 
-    x = array[index]
+  array.length.times do |index|
+   if array[index] < array[index+1]
+   x = array [index]
    end
   }
   x
